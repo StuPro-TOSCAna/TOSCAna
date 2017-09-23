f310ded6a47deba13246744d1cf5d012dbb6a1e1
Chris <halive1996@gmail.com>
2017-09-23 16:41:21 +0200

    Added Spring dependencies for core component
    Also renamed module names to not include the package

--- core/src/test/java/org/opentosca/toscana/core/MainTest.java
+++ core/src/test/java/org/opentosca/toscana/core/MainTest.java
@@ -10,14 +10,13 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(JUnit4.class)
 public class MainTest {
-    private Main main;
+
     @Before
     public void setUp(){
-        main = new Main();
     }
     @Test
     public void testOne(){
-        assertEquals("Hello!",main.sayHello());
+
 
     }
 
