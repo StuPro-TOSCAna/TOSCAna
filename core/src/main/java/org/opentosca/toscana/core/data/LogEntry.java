249fc0cfd1fff688c312bc964689b518698b3145
Heiko Nickerl <dev@heiko-nickerl.com>
2017-09-26 09:54:52 +0200

    exchanged asserts with runtimeexceptions

--- core/src/main/java/org/opentosca/toscana/core/data/LogEntry.java
+++ core/src/main/java/org/opentosca/toscana/core/data/LogEntry.java
@@ -6,11 +6,19 @@ public class LogEntry {
     public final String MESSAGE;
     public final LogLevel LOG_LEVEL;
 
-    LogEntry(long timestamp, String message, LogLevel level){
+    LogEntry(long timestamp, String message, LogLevel level) {
         assert message != null;
         assert level != null;
         this.TIMESTAMP = timestamp;
         this.MESSAGE = message;
         this.LOG_LEVEL = level;
+
+        if (message == null || level == null) {
+            throw new IllegalArgumentException(String.format("LogEntry '%s' is invalid", this));
+        }
+    }
+
+    public String toString() {
+        return String.format("LogEntry [TIMESTAMP='%d', MESSAGE='%s', LOG_LEVEL='%s']");
     }
 }
--- core/src/main/java/org/opentosca/toscana/core/data/Platform.java
+++ core/src/main/java/org/opentosca/toscana/core/data/Platform.java
@@ -13,26 +13,29 @@ public class Platform {
 
     /**
      * Creates a new platform.
-     * @param id short identifier of platform. must match regex [a-z_-]+
-     * @param name displayable name of platform. must not be an empty string
+     *
+     * @param id                 short identifier of platform. must match regex [a-z_-]+
+     * @param name               displayable name of platform. must not be an empty string
      * @param requiredProperties the properties the platform requires.
      */
-    public Platform(String id, String name, Set<Property> requiredProperties){
-        assert id != null && id.matches("[a-z_-]+");
-        assert name != null && !name.isEmpty();
-        assert requiredProperties != null;
+    public Platform(String id, String name, Set<Property> requiredProperties) {
         this.id = id;
         this.name = name;
         this.requiredProperties = requiredProperties;
+        if (id == null || !id.matches("[a-z_-]+") || name == null || name.isEmpty() ||
+                requiredProperties == null) {
+            throw new IllegalArgumentException(String.format("Platform '%s' is invalid", this));
+        }
     }
 
     /**
      * Creates a new platform. Used in case the platform does not require properties.
-     * @param id short identifier of platform
+     *
+     * @param id   short identifier of platform
      * @param name displayable name of platform
      * @see #Platform(String, String, Set<Property>)
      */
-    Platform(String id, String name){
+    Platform(String id, String name) {
         this(id, name, new HashSet<Property>());
     }
 
@@ -40,10 +43,14 @@ public class Platform {
     /**
      * @return a list of properties which are necessary for a transformation to this platform.
      */
-    public List<Property> getRequiredProperties(){
+    public List<Property> getRequiredProperties() {
         // TODO
         // Remember to return a deep copy of the properties..!
         throw new UnsupportedOperationException();
 
+    }
+
+    public String toString() {
+        return String.format("Platform [id='%s', name='%s', requiredProperties='%s']", id, name, requiredProperties);
     }
 }
