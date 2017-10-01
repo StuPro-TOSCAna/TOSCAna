0421b368689da574db76ad2ee359171312a79648
Chris <halive1996@gmail.com>
2017-10-01 17:01:59 +0200

    Added Controllers and Spring HATEOAS Dependency

---
+++ core/src/main/java/org/opentosca/toscana/core/api/CsarController.java
@@ -0,0 +1,9 @@
+package org.opentosca.toscana.core.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/csars")
+public class CsarController {
+}
