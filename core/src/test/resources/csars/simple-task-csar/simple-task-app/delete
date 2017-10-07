ce07e7e0365be4bf4a6224832043d199dd5a6252
hnicke <dev@heiko-nickerl.com>
2017-10-07 18:46:22 +0200

    add operations for app node

---
+++ core/src/test/resources/csars/simple-task-csar/simple-task-app/delete
@@ -0,0 +1,3 @@
+#!/bin/bash  
+docker rm simple-task-app-1 &&
+echo "deleted simple-task-app"
--- core/src/test/resources/csars/simple-task-csar/simple-task-app/mysql-credentials.php
+++ core/src/test/resources/csars/simple-task-csar/simple-task-app/mysql-credentials.php
@@ -1,5 +1,5 @@
 <?php
-$host = "localhost";
+$host = "127.0.0.1";
 $username = "root";
 $password = "";
 $database = "mydb";
