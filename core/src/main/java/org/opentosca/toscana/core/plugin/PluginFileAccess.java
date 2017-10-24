d515d3edbefc5f6d7d706d263030ac9699e3b36c
hnicke <dev@heiko-nickerl.com>
2017-10-25 00:49:38 +0200

    Implement PluginFileAccess

--- core/src/main/java/org/opentosca/toscana/core/plugin/PluginFileAccess.java
+++ core/src/main/java/org/opentosca/toscana/core/plugin/PluginFileAccess.java
@@ -1,27 +1,74 @@
 package org.opentosca.toscana.core.plugin;
 
-import org.opentosca.toscana.core.csar.Csar;
+import org.apache.commons.io.FileUtils;
+import org.opentosca.toscana.core.csar.CsarDao;
 import org.opentosca.toscana.core.transformation.Transformation;
+import org.opentosca.toscana.core.transformation.TransformationDao;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class PluginFileAccess {
-    private File csarRootPath;
-    private File transformationRootPath;
 
-    //TODO Implement mechanism to get the paths
-    public PluginFileAccess(Csar csar, Transformation transformation) {
-        this.csarRootPath = csarRootPath;
-        this.transformationRootPath = transformationRootPath;
+    private final Logger logger;
+    private final File sourceDir;
+    private final File targetDir;
+
+
+    public PluginFileAccess(Transformation transformation, File sourceDir, File targetDir) {
+        logger = transformation.getLog().getLogger(getClass());
+        this.sourceDir = sourceDir;
+        this.targetDir = targetDir;
     }
-    
-    public boolean copy(String relativePath) {
-        //TODO Implement Copy mechanism
-        return false;
+
+    /**
+     * Copies given file or directory from the csar content directory to the transformation directory
+     * If given path specifies a directory, this directory's content gets copied aswell
+     * @param relativePath the path to the source file relative to the csar's content directory
+     * @throws FileNotFoundException if no file or directory was found for given path
+     * @throws IOException if an error occurred while copying
+     */
+    public void copy(String relativePath) throws FileNotFoundException, IOException {
+        File source = new File(sourceDir, relativePath);
+        File target = new File(targetDir, relativePath);
+        if (!source.exists()) {
+            logger.error("Failed to copy '{}': file not found", sourceDir);
+            throw new FileNotFoundException();
+        }
+        try {
+            if (source.isDirectory()) {
+                FileUtils.copyDirectory(source, target);
+                logger.info("Copied directory '{}' and its content to '{}'", sourceDir, targetDir);
+            } else if (source.isFile()){
+                FileUtils.copyFile(source, target);
+                logger.info("Copied file '{}' to '{}'", sourceDir, targetDir);
+            }
+        } catch (IOException e) {
+            logger.error("Failed to copy from '{}' to '{}'", source, target, e);
+            throw e;
+        }
+
+
     }
-    
-    public File createFile(String relativePath) {
-        //TODO Implement creation mechanism
-        return null;
+
+    /**
+     * Writes given InputStream to given path
+     * Creates missing subdirectories.
+     * @param relativePath the path to the target file relative to the transformations root dir
+     * @param inputStream stream which will get written to file
+     */
+    public void write(String relativePath, InputStream inputStream) throws IOException {
+        File target = new File(targetDir, relativePath);
+        try {
+            FileUtils.copyInputStreamToFile(inputStream, target);
+            logger.info("Written stream to new file '{}'", target);
+        } catch (IOException e){
+            logger.error("Failed to write stream to '{}'", target);
+            throw e;
+        }
     }
 }
--- core/src/main/java/org/opentosca/toscana/core/transformation/TransformationContext.java
+++ core/src/main/java/org/opentosca/toscana/core/transformation/TransformationContext.java
@@ -5,14 +5,16 @@ import org.opentosca.toscana.core.plugin.PluginFileAccess;
 import org.opentosca.toscana.core.transformation.properties.PropertyInstance;
 import org.slf4j.Logger;
 
+import java.io.File;
+
 public final class TransformationContext {
     private final Transformation transformation;
     private final PluginFileAccess access;
 
 
-    public TransformationContext(Transformation transformation) {
+    public TransformationContext(Transformation transformation, File csarContentDir, File transformationRootDir) {
         this.transformation = transformation;
-        this.access = new PluginFileAccess(transformation.getCsar(), transformation);
+        this.access = new PluginFileAccess(transformation, csarContentDir, transformationRootDir);
 
     }
 
