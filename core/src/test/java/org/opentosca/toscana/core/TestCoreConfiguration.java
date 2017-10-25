49fa58696446e5066d02b24abf90f00524ab1441
Chris <halive1996@gmail.com>
2017-10-26 01:44:56 +0200

    Fixed #86
    The Default properties are now overridden when using the "BaseSpringTest". Currently it just uses the classpath root, should be good for testing though!

--- core/src/test/java/org/opentosca/toscana/core/TestCoreConfiguration.java
+++ core/src/test/java/org/opentosca/toscana/core/TestCoreConfiguration.java
@@ -31,6 +31,7 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.TestPropertySource;
 
 @Configuration
 @PropertySource("classpath:application.yml")
