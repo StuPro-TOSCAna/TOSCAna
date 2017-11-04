25d1ba3473ae1609fee9fcc33c0d58947af04a7b
Chris <halive1996@gmail.com>
2017-11-04 05:21:34 +0100

    Fixed Upload test
    Created Abstract class BaseIntegrationTest to externalize properties common with these types of tests
    Remove some random file in the repository
    
    The Upload test now uses the integrated Spring Tools to launch in a regular Application Context

---
+++ core/src/test/java/org/opentosca/toscana/core/testdata/TestProfiles.java
@@ -0,0 +1,9 @@
+package org.opentosca.toscana.core.testdata;
+
+public class TestProfiles {
+    /**
+     This Profile disables the Test core Configuration, in order to allow the 
+     regular application context to get loaded.
+     */
+    public static final String INTEGRATION_TEST_PROFILE = "controller_test";
+}
