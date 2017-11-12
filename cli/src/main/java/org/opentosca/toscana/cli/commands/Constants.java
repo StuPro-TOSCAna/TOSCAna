4d5829490182be92f859529a985ad8f43748f205
Oliver Ferch <oferch@gmx.de>
2017-11-12 21:15:08 +0100

    changed API Port to 5327

--- cli/src/main/java/org/opentosca/toscana/cli/commands/Constants.java
+++ cli/src/main/java/org/opentosca/toscana/cli/commands/Constants.java
@@ -5,6 +5,7 @@ package org.opentosca.toscana.cli.commands;
  */
 public final class Constants {
 
+    public final Integer API_PORT = 5327;
     public static final String PARAM_CSAR = "<CSAR>";
     public static final String PARAM_PLATFORM = "<Platform>";
     public final String CSAR_UPLOAD_SUCCESS = "Upload of CSAR Archive was successful!";
