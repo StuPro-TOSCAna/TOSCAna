c400ce3b9362ac5cfeba6162cfb3c4afa304c792
jmuell <jmuell.dev@gmail.com>
2017-11-15 17:44:32 +0100

    add create_db script

---
+++ server/src/test/resources/csars/yaml/valid/lamp-csar/scripts/db_create.sh
@@ -0,0 +1,7 @@
+# Setup MySQL root password and create user
+cat << EOF | mysql -u root --password=db_root_password
+CREATE DATABASE mydb;
+USE mydb;
+create table tasks (id INT not null auto_increment,task varchar(255), primary key(id));
+EXIT
+EOF
