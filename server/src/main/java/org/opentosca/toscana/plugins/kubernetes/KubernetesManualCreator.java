0e5c458378c229d324cdd0d38459ad394a9b8b0d
Nick Fode <nickfode@posteo.de>
2017-11-15 23:28:01 +0100

    Fix failing transformation using server jar
    Apparently you can not use files to acces the resources when the
    application is packaged as jar. You have to use a InputStream instead.

--- server/src/main/java/org/opentosca/toscana/plugins/kubernetes/KubernetesManualCreator.java
+++ server/src/main/java/org/opentosca/toscana/plugins/kubernetes/KubernetesManualCreator.java
@@ -1,19 +1,19 @@
 package org.opentosca.toscana.plugins.kubernetes;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
-import org.apache.commons.io.Charsets;
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 public class KubernetesManualCreator {
     /**
-     * This method fills the app name and the kubernetes resource file name into the generic template
+     This method fills the app name and the kubernetes resource file name into the generic template
      */
     public static String createManual(String appName, String resFileName) throws IOException {
         Class clazz = KubernetesManualCreator.class;
-        File manual = new File(clazz.getResource("/kubernetes/k8s_manual_guide.md").getPath());
-        String result = FileUtils.readFileToString(manual, Charsets.UTF_8);
+        InputStream manual = clazz.getResourceAsStream("/kubernetes/k8s_manual_guide.md");
+        String result = IOUtils.toString(manual);
+        manual.close();
         result = result.replaceAll("\\{app_name}", appName);
         result = result.replaceAll("\\{k8s_resource_file}", resFileName);
         return result;
