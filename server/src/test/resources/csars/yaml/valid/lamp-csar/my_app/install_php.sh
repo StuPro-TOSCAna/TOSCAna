e8821fc6abab35e6a47bf0e08b9efadad198c47c
Marvin Munoz Baron <mmunozba@outlook.com>
2017-11-16 19:08:13 +0100

    Add no-input LAMP-CSAR
    Add CSAR-files for LAMP CSARs
    include mysql-credentials
    Fix no-input LAMP-CSAR
    Change package install in one line
    Delete duplicate folder and copy second template
    Add update&upgrade to install scripts
    Exchange deprecated version of starting service
    Delete unnecessary .service
    Update msql configure
    Create two seperate folders for input & NoInput
    Use environment variables in script to enable input
    Added input references and removed unnecessary references to concrete implementaion of scripts
    Deleted unnecessary files
    Changed output to full URL

--- server/src/test/resources/csars/yaml/valid/lamp-csar/my_app/install_php.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# install php on a linux machine with php-mysql
-sudo apt-get install php -y
-sudo apt-get install php-mysql -y
---
+++ server/src/test/resources/csars/yaml/valid/lamp-csar_Input/my_app/mysql-credentials.php
@@ -0,0 +1,7 @@
+<?php
+$db_host = getenv('database_host');
+$db_user = "root";
+$db_password = getenv('database_password');
+$db_name = getenv('database_name');
+$db_port = getenv('database_port');
+?>
