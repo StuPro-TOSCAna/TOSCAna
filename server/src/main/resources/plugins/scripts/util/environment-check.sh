f2b50accd6fade67d6aa8ce55084d6d097f8c643
Nick Fode <nickfode@posteo.de>
2017-11-20 22:40:05 +0100

    Fix shell script

--- server/src/main/resources/plugins/scripts/util/environment-check.sh
+++ server/src/main/resources/plugins/scripts/util/environment-check.sh
@@ -2,7 +2,7 @@
 # $1: basename of program
 function check () {
   echo "Check if "$1" is available."                                                      
-  if ! [ -x "$(command -v $1)" ]; then                                                 
+  if ! [ -x "$(command -v "$1")" ]; then                                                 
     echo "Error: "$1" is not installed." >&2                                             
     return 1                                                                                 
   fi
