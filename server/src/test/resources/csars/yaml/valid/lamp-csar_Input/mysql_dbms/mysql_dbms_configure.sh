86c5d316674a1f47cd08f46d632eac9be8bb68b4
jmuell <jmuell.dev@gmail.com>
2017-11-20 13:54:16 +0100

    Deleted unnecessary files

--- server/src/test/resources/csars/yaml/valid/lamp-csar_Input/mysql_dbms/mysql_dbms_configure.sh
+++ server/src/test/resources/csars/yaml/valid/lamp-csar_Input/mysql_dbms/mysql_dbms_configure.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat << EOF | mysql -u root --password=${my_mysql_rootpw}
+cat << EOF | mysql -u root --password=${database_password}
 CREATE DATABASE mydb;
 USE mydb;
 create table tasks (id INT not null auto_increment,task varchar(255), primary key(id));
