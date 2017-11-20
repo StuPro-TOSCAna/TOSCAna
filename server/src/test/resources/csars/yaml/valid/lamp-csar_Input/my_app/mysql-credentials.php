8f23b3318b9a86c1da24e3909fa07950d0c7db58
jmuell <jmuell.dev@gmail.com>
2017-11-20 13:35:24 +0100

    Use environment variables in script to enable input

--- server/src/test/resources/csars/yaml/valid/lamp-csar_Input/my_app/mysql-credentials.php
+++ server/src/test/resources/csars/yaml/valid/lamp-csar_Input/my_app/mysql-credentials.php
@@ -1,7 +1,7 @@
 <?php
-$db_host = "127.0.0.1";
+$db_host = getenv('database_host');
 $db_user = "root";
-$db_password = "abc";
-$db_name = "mydb";
-$db_port = 3306;
+$db_password = getenv('my_mysql_rootpw');
+$db_name = getenv('database_name');
+$db_port = getenv('database_port');
 ?>
--- server/src/test/resources/csars/yaml/valid/lamp-csar_Input/mysql_dbms/mysql_dbms_configure.sh
+++ server/src/test/resources/csars/yaml/valid/lamp-csar_Input/mysql_dbms/mysql_dbms_configure.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat << EOF | mysql -u root --password=abc
+cat << EOF | mysql -u root --password=${my_mysql_rootpw}
 CREATE DATABASE mydb;
 USE mydb;
 create table tasks (id INT not null auto_increment,task varchar(255), primary key(id));
