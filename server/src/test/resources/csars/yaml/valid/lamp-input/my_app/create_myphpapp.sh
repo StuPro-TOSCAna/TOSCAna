b0172e36d00d061851966b51b9295fd1e35e8a5a
Christian Müller <halive1996@gmail.com>
2017-11-28 15:25:22 +0100

    Revert "Create LAMP CSAR (#211)"
    This reverts commit d1e47d4fdbf7907c7b37f213ced7791069cf9075.

--- server/src/test/resources/csars/yaml/valid/lamp-input/my_app/create_myphpapp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# install php on a linux machine with php-mysql
-sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt-get install -y php php-mysql libapache2-mod-php7.0
-
-sudo mv myphpapp.php /var/www/html/
-sudo mv mysql-credentials.php /var/www/html/
-
