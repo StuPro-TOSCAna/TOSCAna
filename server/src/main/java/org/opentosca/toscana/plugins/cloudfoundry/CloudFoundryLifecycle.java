ce8d1b26bf8a3c9803a39e33b75d1064b8e012d8
jmuell <jmuell.dev@gmail.com>
2017-11-29 17:46:30 +0100

    Delete additional context

--- server/src/main/java/org/opentosca/toscana/plugins/cloudfoundry/CloudFoundryLifecycle.java
+++ server/src/main/java/org/opentosca/toscana/plugins/cloudfoundry/CloudFoundryLifecycle.java
@@ -7,11 +7,8 @@ import org.opentosca.toscana.plugins.lifecycle.AbstractLifecycle;
 
 public class CloudFoundryLifecycle extends AbstractLifecycle {
 
-    private final TransformationContext context;
-
     public CloudFoundryLifecycle(TransformationContext context) throws IOException {
         super(context);
-        this.context = context;
     }
 
     @Override
