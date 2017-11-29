70c17262b54a68b3422d34b77b51e15283185f47
jmuell <jmuell.dev@gmail.com>
2017-11-29 17:43:09 +0100

    Improve code-quality

--- server/src/main/java/org/opentosca/toscana/plugins/cloudfoundry/CloudFoundryApplication.java
+++ server/src/main/java/org/opentosca/toscana/plugins/cloudfoundry/CloudFoundryApplication.java
@@ -2,19 +2,20 @@ package org.opentosca.toscana.plugins.cloudfoundry;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+/**
+ This class should describe a CloudFoundryApplication with all needed information to deploy it
+ */
 public class CloudFoundryApplication {
-    //private final static Logger logger = LoggerFactory.getLogger(CloudFoundryApplication.class);
+    
     private String appName;
-    private Map<String,String> envVariables = new HashMap<>();
-    private ArrayList<String> services = new ArrayList<>();
-    private ArrayList<String> bpAdditions = new ArrayList<>();
+    private final Map<String, String> environmentVariables = new HashMap<>();
+    private final ArrayList<String> services = new ArrayList<>();
+    private final ArrayList<String> bpAdditions = new ArrayList<>();
 
-    public CloudFoundryApplication(String appName){
+    public CloudFoundryApplication(String appName) {
         this.appName = appName;
     }
 
@@ -22,19 +23,15 @@ public class CloudFoundryApplication {
         return appName;
     }
 
-    public void setAppName(String appName) {
-        this.appName = appName;
+    public Map<String, String> getEnvironmentVariables() {
+        return environmentVariables;
     }
 
-    public Map<String, String> getEnvVariables() {
-        return envVariables;
+    public void addEnvironmentVariables(String environmentVariableName, String value) {
+        this.environmentVariables.put(environmentVariableName, value);
     }
 
-    public void addEnvVar(String envName, String value) {
-        this.envVariables.put(envName, value);
-    }
-
-    public ArrayList<String> getServices() {
+    public List<String> getServices() {
         return services;
     }
 
@@ -42,13 +39,11 @@ public class CloudFoundryApplication {
         this.services.add(serviceName);
     }
 
-    public ArrayList<String> getBpAdditions() {
+    public List<String> getBuildpackAdditions() {
         return bpAdditions;
     }
 
-    public void addBp(String buildPack) {
+    public void addBuildpack(String buildPack) {
         this.bpAdditions.add(buildPack);
     }
-
-
 }
--- server/src/main/java/org/opentosca/toscana/plugins/cloudfoundry/CloudFoundryFileCreator.java
+++ server/src/main/java/org/opentosca/toscana/plugins/cloudfoundry/CloudFoundryFileCreator.java
@@ -5,68 +5,67 @@ import java.util.Map;
 
 import org.opentosca.toscana.core.plugin.PluginFileAccess;
 
+/**
+ Creates all files which are necessary to deploy the application
+ Files: manifest.yml, builpack additions, deployScript
+ */
 public class CloudFoundryFileCreator {
 
-    private static PluginFileAccess fileAccess;
-    private CloudFoundryApplication app;
+    private final PluginFileAccess fileAccess;
+    private final CloudFoundryApplication app;
 
-    public CloudFoundryFileCreator(PluginFileAccess fileAccess, CloudFoundryApplication app){
-        this.fileAccess=fileAccess;
-        this.app=app;
+    public CloudFoundryFileCreator(PluginFileAccess fileAccess, CloudFoundryApplication app) {
+        this.fileAccess = fileAccess;
+        this.app = app;
     }
 
-    public void createFiles() throws IOException{
+    public void createFiles() throws IOException {
         createManifest();
-        createBpConfig();
-        createDeploySh();
+        createBuildpackAdditionsFile();
+        createDeployScript();
     }
 
-    private void createManifest() throws IOException{
+    private void createManifest() throws IOException {
         String manifestContent;
-        manifestContent=createHeadMani();
-        manifestContent+=createEnv();
-        manifestContent+=createService();
+        manifestContent = createManifestHead();
+        manifestContent += createEnvironmentVariables();
+        manifestContent += createService();
         fileAccess.access("/manifest.yml").append(manifestContent).close();
     }
 
-    private String createHeadMani(){
-        return "applications: \n- name: "+app.getAppName();
+    private String createManifestHead() {
+        return "applications: \n- name: " + app.getAppName();
     }
 
-    private String createEnv(){
+    private String createEnvironmentVariables() {
         String envBlock;
-        envBlock="\n  env:";
-        for (Map.Entry<String, String> entry : app.getEnvVariables().entrySet()){
-            envBlock=envBlock+"\n    " +entry.getKey()+": "+entry.getValue();
+        envBlock = "\n  env:";
+        for (Map.Entry<String, String> entry : app.getEnvironmentVariables().entrySet()) {
+            envBlock = envBlock + "\n    " + entry.getKey() + ": " + entry.getValue();
         }
         return envBlock;
     }
 
-    private String createService(){
+    private String createService() {
         String serviceBlock;
-        serviceBlock="\n  service:";
-        for (String service : app.getServices()){
-            serviceBlock+="\n    - "+service;
+        serviceBlock = "\n  service:";
+        for (String service : app.getServices()) {
+            serviceBlock += "\n    - " + service;
         }
         return serviceBlock;
     }
 
-
-    private void createDeploySh() throws IOException{
-        fileAccess.access("/deploy_"+app.getAppName()+".sh")
-            .append("cf -push "+fileAccess.getAbsolutePath("/")+app.getAppName()).close();
+    private void createDeployScript() throws IOException {
+        fileAccess.access("/deploy_" + app.getAppName() + ".sh")
+            .append("cf -push " + app.getAppName()).close();
     }
 
-    private void createBpConfig() throws IOException{
-        String buildPackAdditions="{\"PHP-EXTENSIONS\":[";
-        for(String bp : app.getBpAdditions()){
-            buildPackAdditions+="\""+bp+"\",";
+    private void createBuildpackAdditionsFile() throws IOException {
+        String buildPackAdditions = "{\"PHP-EXTENSIONS\":[";
+        for (String bp : app.getBuildpackAdditions()) {
+            buildPackAdditions += "\"" + bp + "\",";
         }
-        buildPackAdditions+="]}";
-        fileAccess.createDirectories("/.bp-config");
+        buildPackAdditions += "]}";
         fileAccess.access("/.bp-config/options.json").append(buildPackAdditions).close();
     }
-
-
-
 }
