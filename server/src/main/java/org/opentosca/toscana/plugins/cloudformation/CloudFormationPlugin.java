a6dfe3ca93bd90b76bbd300f995bafac1fd72a24
Marvin Munoz Baron <mmunozba@outlook.com>
2017-11-30 04:54:53 +0100

    Add CloudFormationLifecycle

--- server/src/main/java/org/opentosca/toscana/plugins/cloudformation/CloudFormationPlugin.java
+++ server/src/main/java/org/opentosca/toscana/plugins/cloudformation/CloudFormationPlugin.java
@@ -3,16 +3,19 @@ package org.opentosca.toscana.plugins.cloudformation;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.opentosca.toscana.core.plugin.AbstractPlugin;
 import org.opentosca.toscana.core.transformation.TransformationContext;
 import org.opentosca.toscana.core.transformation.platform.Platform;
 import org.opentosca.toscana.core.transformation.properties.Property;
+import org.opentosca.toscana.plugins.lifecycle.LifecycleAwarePlugin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CloudFormationPlugin extends AbstractPlugin {
-
+public class CloudFormationPlugin extends LifecycleAwarePlugin<CloudFormationLifecycle> {
+    private final static Logger logger = LoggerFactory.getLogger(CloudFormationPlugin.class);
+    
     public CloudFormationPlugin() {
         super(getPlatformDetails());
     }
@@ -27,5 +30,10 @@ public class CloudFormationPlugin extends AbstractPlugin {
         String platformName = "AWS CloudFormation";
         Set<Property> platformProperties = new HashSet<>();
         return new Platform(platformId, platformName, platformProperties);
+    }
+
+    @Override
+    protected CloudFormationLifecycle getInstance(TransformationContext context) throws Exception {
+        return new CloudFormationLifecycle(context);
     }
 }
