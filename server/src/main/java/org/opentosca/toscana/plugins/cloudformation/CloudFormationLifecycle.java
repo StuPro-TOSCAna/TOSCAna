ad9e00b23a7d28d13568620f4af509b6dc7919e9
Marvin Munoz Baron <mmunozba@outlook.com>
2017-11-30 11:42:37 +0100

    Add CloudFormationLifecycle (#288)

---
+++ server/src/main/java/org/opentosca/toscana/plugins/cloudformation/CloudFormationLifecycle.java
@@ -0,0 +1,33 @@
+package org.opentosca.toscana.plugins.cloudformation;
+
+import java.io.IOException;
+
+import org.opentosca.toscana.core.transformation.TransformationContext;
+import org.opentosca.toscana.plugins.lifecycle.AbstractLifecycle;
+
+public class CloudFormationLifecycle extends AbstractLifecycle {
+
+    public CloudFormationLifecycle(TransformationContext context) throws IOException {
+        super(context);
+    }
+
+    @Override
+    public boolean checkModel() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void prepare() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void transform() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void cleanup() {
+        throw new UnsupportedOperationException();
+    }
+}
