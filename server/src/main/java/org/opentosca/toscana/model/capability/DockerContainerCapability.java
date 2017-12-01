ee86ac7a6aacb19330f31c20767e839f1bc8ced0
Heiko Nickerl <dev@heiko-nickerl.com>
2017-12-01 13:30:32 +0100

    Remove obsolete builder() in DockerContainerCapability

--- server/src/main/java/org/opentosca/toscana/model/capability/DockerContainerCapability.java
+++ server/src/main/java/org/opentosca/toscana/model/capability/DockerContainerCapability.java
@@ -91,10 +91,6 @@ public class DockerContainerCapability extends ContainerCapability {
         this.volumeId = volumeId;
     }
     
-    public static DockerContainerCapabilityBuilder builder() {
-        return new DockerContainerCapabilityBuilder();
-    }
-
     /**
      @return {@link #hostId}
      */
