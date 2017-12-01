77cba81fac3ed1f21b6ca4727d2b595fecbf685a
Heiko Nickerl <dev@heiko-nickerl.com>
2017-12-01 01:31:18 +0100

    Add first bit of convert logic

--- server/src/main/java/org/opentosca/toscana/core/parse/ModelConverter.java
+++ server/src/main/java/org/opentosca/toscana/core/parse/ModelConverter.java
@@ -7,25 +7,35 @@ import java.util.Set;
 import org.opentosca.toscana.model.EffectiveModel;
 import org.opentosca.toscana.model.node.RootNode;
 
-import org.eclipse.winery.model.tosca.yaml.TNodeType;
+import org.eclipse.winery.model.tosca.yaml.TNodeTemplate;
 import org.eclipse.winery.model.tosca.yaml.TServiceTemplate;
+import org.eclipse.winery.model.tosca.yaml.TTopologyTemplateDefinition;
 
 /**
  Contains logic to convert a {@link TServiceTemplate} to a {@link EffectiveModel}
  */
 public class ModelConverter {
-    
+
     public EffectiveModel convert(TServiceTemplate serviceTemplate) {
-        Set<RootNode> nodes = convertNodeTemplates(serviceTemplate);
+        Set<RootNode> nodes = convertNodeTemplates(serviceTemplate.getTopologyTemplate());
         EffectiveModel model = new EffectiveModel(nodes);
         return model;
     }
 
-    private Set<RootNode> convertNodeTemplates(TServiceTemplate serviceTemplate) {
+    private Set<RootNode> convertNodeTemplates(TTopologyTemplateDefinition topology) {
         Set<RootNode> nodes = new HashSet<>();
-        Map<String, TNodeType> nodeMap = serviceTemplate.getNodeTypes();
-        // TODO
+        Map<String, TNodeTemplate> nodeMap = topology.getNodeTemplates();
+        nodeMap.forEach((nodeName, node) -> nodes.add(convertNodeTemplate(nodeName, node)));
         return nodes;
+    }
+
+    private RootNode convertNodeTemplate(String nodeName, TNodeTemplate node) {
+        switch (node.getType().getLocalPart()) {
+            case(""):
+                // TODO
+                break;
+        }
+        return null;
     }
 
 }
