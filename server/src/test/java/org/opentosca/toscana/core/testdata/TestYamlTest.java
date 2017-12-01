fccf3979a194f5aa333221a1c8201e4d068a56fe
Heiko Nickerl <dev@heiko-nickerl.com>
2017-12-01 20:27:01 +0100

    Upgrade to latest snapshot of winery parser

--- server/src/test/java/org/opentosca/toscana/core/testdata/TestYamlTest.java
+++ server/src/test/java/org/opentosca/toscana/core/testdata/TestYamlTest.java
@@ -2,28 +2,24 @@ package org.opentosca.toscana.core.testdata;
 
 import java.io.File;
 
-import org.opentosca.toscana.core.BaseJUnitTest;
+import org.opentosca.toscana.core.BaseSpringTest;
+import org.opentosca.toscana.core.parse.CsarParseService;
 
-import org.eclipse.winery.yaml.common.exception.MultiException;
-import org.eclipse.winery.yaml.common.reader.yaml.Reader;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  Contains integration tests for our yaml models used to test our application
  */
-public class TestYamlTest extends BaseJUnitTest {
-    @Test
-    public void testMinimalDocker() throws MultiException {
-        File minimalDocker = new File(TestCsars.YAML_DIR, "valid/minimal-docker/minimal-docker.yaml");
-        parseFile(minimalDocker.getParent(), minimalDocker.getName());
-    }
+public class TestYamlTest extends BaseSpringTest {
 
-    /**
-     Utility that parses a yaml file
-     with the help of the winery yaml reader
-     */
-    private void parseFile(String path, String name) throws MultiException {
-        Reader yamlReader = new Reader();
-        yamlReader.parse(path, name);
+    @Autowired
+    private CsarParseService parser;
+
+    @Test
+    public void testMinimalDocker() throws Exception {
+        File minimalDocker = new File(TestCsars.YAML_DIR, "valid/minimal-docker/minimal-docker.yaml");
+        parser.parse(minimalDocker);
+
     }
 }
