a58c49d0c2b6d210eecdfa80fcb9d2cc1a95382e
jmuell <jmuell.dev@gmail.com>
2017-12-02 17:31:21 +0100

    Add reference comment

--- server/src/main/java/org/opentosca/toscana/plugins/scripts/EnvironmentCheck.java
+++ server/src/main/java/org/opentosca/toscana/plugins/scripts/EnvironmentCheck.java
@@ -2,10 +2,11 @@ package org.opentosca.toscana.plugins.scripts;
 
 /**
  checks if a given environment exists 
+ wraps resources/plugins.scripts.util/environment-check.sh
  */
 public class EnvironmentCheck {
         
-    public static String checkEnvironment(String command){
+    public static String checkEnvironment(String command) {
         return "check \"" + command + "\"";
     }
     
