c7e961e8c4b2e15f743b30e88f723f3d3da990e2
Marvin Munoz Baron <mmunozba@outlook.com>
2017-12-04 16:55:28 +0100

    Add CFBuilderTest

---
+++ server/src/test/java/org/opentosca/toscana/plugins/cloudformation/CFBuilderTest.java
@@ -0,0 +1,98 @@
+package org.opentosca.toscana.plugins.cloudformation;
+
+import javax.xml.crypto.dsig.keyinfo.KeyName;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.scaleset.cfbuilder.core.CloudFormationJsonModule;
+import com.scaleset.cfbuilder.core.Module;
+import com.scaleset.cfbuilder.core.Ref;
+import com.scaleset.cfbuilder.core.Tag;
+import com.scaleset.cfbuilder.core.Template;
+import com.scaleset.cfbuilder.core.Resource;
+import com.scaleset.cfbuilder.ec2.Instance;
+import com.scaleset.cfbuilder.ec2.SecurityGroup;
+import com.scaleset.cfbuilder.ec2.SecurityGroupIngress;
+import org.junit.Test;
+
+import static com.scaleset.cfbuilder.ec2.SecurityGroup.PortRange.range;
+import static org.junit.Assert.assertNotNull;
+
+public class CFBuilderTest extends Module {
+    
+    @Test
+    public void simpleTest() throws Exception {
+        Template lampTemplate = new Template();
+//        Module testModule = new Module();
+        
+        // Build Resources
+//        testModule.resource(WebServerInstance.class, "WebServerInstance")
+//            .name
+//        testModule.resource(WebServerInstance.class, "WebServerInstance").na;
+        // Set Parameters
+//        lampTemplate.strParam("KeyName").description("Name of an existing EC2 KeyPair to enable SSH access to the instances").type("AWS::EC2::KeyPair::KeyName").constraintDescription("must be the name of an existing EC2 KeyPair.");
+//        // Set Resources
+//        lampTemplate.resource(WebServerInstance.class, ns("WebServerInstance"));
+        
+        
+        new CFBuilderTest.TestModule().id("").template(lampTemplate).build();
+
+        assertNotNull(lampTemplate);
+        System.err.println(lampTemplate.toString());
+        
+//        ObjectMapper mapper = new ObjectMapper().registerModule(new CloudFormationJsonModule().scanTypes());
+//        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.UPPER_CAMEL_CASE);
+//
+//        Template template = mapper.readValue(lampTemplate.toString(), Template.class);
+//        assertNotNull(template);
+//        System.err.println(template.toString());
+    }
+
+    class TestModule extends Module {
+
+        public void build() throws Exception {
+            
+            Object keyName = option("KeyName").orElseGet(
+                () -> strParam("KeyName").type("AWS::EC2::KeyPair::KeyName").description("Name of an existing EC2 KeyPair to enable SSH access to the instances").constraintDescription("must be the name of an existing EC2 KeyPair."));
+            
+            Object clusterName = option("clusterName").orElse("elasticsearch");
+            Object cidrIp = "0.0.0.0/0";
+            Object keyNameVar = template.ref("KeyName");
+            Object imageId = template.ref("ImageId");
+            Object az = template.ref("MyAZ");
+            Object instanceProfile = ref("InstanceProfile");
+            Object vpcId = template.ref("VpcId");
+
+            SecurityGroup webServerSecurityGroup = resource(SecurityGroup.class, "WebServerSecurityGroup")
+                .ingress(ingress -> ingress.cidrIp(cidrIp), "tcp", 80, 22);
+
+            Object groupId = webServerSecurityGroup.fnGetAtt("GroupId");
+
+            resource(SecurityGroupIngress.class, "SelfReferenceIngress")
+                .sourceSecurityGroupId(groupId)
+                .groupId(groupId)
+                .ipProtocol("tcp")
+                .port(9300);
+
+            resource(Instance.class, "Instance")
+                .name(ns("Instance"))
+                .availabilityZone(az)
+                .keyName(keyNameVar)
+                .imageId(imageId)
+                .instanceProfile(instanceProfile)
+                .instanceType(keyName)
+                .securityGroupIds(webServerSecurityGroup);
+            
+            resource(Instance.class, "WebServerInstance")
+                .imageId("ami-0def3275")
+                .instanceType("t2.micro")
+                .securityGroupIds(webServerSecurityGroup)
+                .keyName(keyName);
+            
+            Object value = option("Value");
+            
+            output("websiteURL", value, "URL for newly created LAMP stack");
+        }
+    }
+}
