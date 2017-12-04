618f9b2456bd4914fe4401a0fd98a45e30cad78e
jmuell <jmuell.dev@gmail.com>
2017-12-05 00:53:11 +0100

    Add java-cf-client and start implementation

--- server/src/main/java/org/opentosca/toscana/plugins/cloudfoundry/CloudFoundryLifecycle.java
+++ server/src/main/java/org/opentosca/toscana/plugins/cloudfoundry/CloudFoundryLifecycle.java
@@ -1,11 +1,13 @@
 package org.opentosca.toscana.plugins.cloudfoundry;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.Set;
 
 import org.opentosca.toscana.core.transformation.TransformationContext;
 import org.opentosca.toscana.model.node.RootNode;
 import org.opentosca.toscana.model.visitor.VisitableNode;
+import org.opentosca.toscana.plugins.cloudfoundry.CloudFoundryClient.CloudFoundryConnection;
 import org.opentosca.toscana.plugins.cloudfoundry.visitors.CloudFoundryNodeVisitor;
 import org.opentosca.toscana.plugins.lifecycle.AbstractLifecycle;
 
@@ -26,11 +28,25 @@ public class CloudFoundryLifecycle extends AbstractLifecycle {
     @Override
     public void prepare() {
         //throw new UnsupportedOperationException();
+
+        //TODO: check if cf cli is installed
+        //TODO: check if properties are filled
+        Map<String, String> properties = context.getProperties().getPropertyValues();
+        String username = properties.get("username");
+        String password = properties.get("password");
+        String organization = properties.get("organization");
+        String space = properties.get("space");
+        String apiHost = properties.get("apihost");
+
+        CloudFoundryConnection cloudFoundryConnection = new CloudFoundryConnection(username, password,
+            apiHost, organization, space);
+
+        //TODO: Do something with connection
+
     }
 
     @Override
     public void transform() {
-
         CloudFoundryApplication myApp = new CloudFoundryApplication();
         CloudFoundryNodeVisitor visitor = new CloudFoundryNodeVisitor(myApp);
         Set<RootNode> nodes = context.getModel().getNodes();
