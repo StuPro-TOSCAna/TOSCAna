0b526f388277b045bc1014c04b54dfe79bd22f9b
jmuell <jmuell.dev@gmail.com>
2018-01-12 01:05:35 +0100

    Change db_user to a environment variable in common Lamp (#401)
    * Change db_user to a environment variable
    * Adapte template.yaml
    * Adapt template and add missing ";" to myphpapp
    * Change 'localhost' to '127.0.0.1'

--- server/src/test/resources/csars/yaml/valid/lamp-input/my_app/configure_myphpapp.sh
+++ server/src/test/resources/csars/yaml/valid/lamp-input/my_app/configure_myphpapp.sh
@@ -5,3 +5,4 @@ sed -i "s:DATABASE_HOST:${database_host}:g" $CREDENTIALS
 sed -i "s:DATABASE_PASSWORD:${database_password}:g" $CREDENTIALS
 sed -i "s:DATABASE_NAME:${database_name}:g" $CREDENTIALS
 sed -i "s:DATABASE_PORT:${database_port}:g" $CREDENTIALS
+sed -i "s:DATABASE_USER:${database_user}:g" $CREDENTIALS
