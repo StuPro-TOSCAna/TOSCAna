fec955b3617a9fb33f79b6f6dbd5fd2203f83ba9
Nick Fode <nickfode@posteo.de>
2018-01-16 00:56:20 +0100

    Add api

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { PlatformsService } from './api/api/platforms.service';
 import { Component } from '@angular/core';
+import { log } from 'util';
 
 @Component({
     selector: 'app-root',
@@ -7,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'app';
+    test = '';
+    constructor() {
+    }
 }
