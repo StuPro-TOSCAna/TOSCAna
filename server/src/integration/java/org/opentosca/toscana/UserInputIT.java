8138c2d5e3226fc6a80bd7150b97a33d3aa94180
Heiko Nickerl <dev@heiko-nickerl.com>
2018-01-20 16:23:42 +0100

    Fix UserInputTest

--- server/src/integration/java/org/opentosca/toscana/UserInputIT.java
+++ server/src/integration/java/org/opentosca/toscana/UserInputIT.java
@@ -1,21 +1,17 @@
 package org.opentosca.toscana;
 
 import java.io.IOException;
-import java.util.Map;
 
 import org.opentosca.toscana.core.BaseSpringIntegrationTest;
 import org.opentosca.toscana.core.testdata.TestCsars;
+import org.opentosca.toscana.plugins.cloudformation.CloudFormationPlugin;
 import org.opentosca.toscana.plugins.cloudfoundry.CloudFoundryPlugin;
 import org.opentosca.toscana.retrofit.TOSCAnaAPI;
-import org.opentosca.toscana.retrofit.model.Csar;
-import org.opentosca.toscana.retrofit.model.Transformation;
 import org.opentosca.toscana.retrofit.model.TransformationProperties;
 import org.opentosca.toscana.retrofit.model.TransformationProperty;
 import org.opentosca.toscana.retrofit.util.TOSCAnaServerException;
 
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  Tests whether user inputs are correctly propagated into the EffectiveModel and provided for the transformation.
@@ -23,16 +19,23 @@ import org.slf4j.LoggerFactory;
 public class UserInputIT extends BaseSpringIntegrationTest {
 
     @Test
-    public void transformationWithModelInputs() throws IOException, TOSCAnaServerException {
+    public void transformationWithModelInputs() throws IOException, TOSCAnaServerException, InterruptedException {
         TOSCAnaAPI api = new TOSCAnaAPI(getHttpUrl());
         String csarName = "lamp";
         api.uploadCsar(csarName, TestCsars.VALID_LAMP_INPUT);
         String platformId = new CloudFoundryPlugin().getPlatform().id;
         api.createTransformation(csarName, platformId);
         TransformationProperties properties = api.getProperties(csarName, platformId);
-        for (TransformationProperty property : properties.getProperties()){
+        for (TransformationProperty property : properties.getProperties()) {
             property.setValue("1");
         }
+        api.updateProperties(csarName, platformId, properties);
+        // if call does not fail, inputs have been correctly set and propagated
         api.startTransformation(csarName, platformId);
+        
+        // wait for transformation is completed - else cleanup will kill transformation
+        while ("TRANSFORMING".matches(api.getTransformation(csarName, platformId).getStatus())) {
+            Thread.sleep(20);
+        }
     }
 }
