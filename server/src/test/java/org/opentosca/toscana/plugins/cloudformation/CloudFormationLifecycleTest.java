170ccf1619168d793527e0aad980cba37f0b9426
Manuel Kloppenburg <mklopp@outlook.de>
2018-01-26 15:01:58 +0100

    Fix test

--- server/src/test/java/org/opentosca/toscana/plugins/cloudformation/CloudFormationLifecycleTest.java
+++ server/src/test/java/org/opentosca/toscana/plugins/cloudformation/CloudFormationLifecycleTest.java
@@ -11,6 +11,7 @@ import org.opentosca.toscana.core.transformation.TransformationContext;
 import org.opentosca.toscana.core.transformation.properties.PropertyInstance;
 import org.opentosca.toscana.model.EffectiveModel;
 
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -24,6 +25,8 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 import static org.opentosca.toscana.plugins.cloudformation.CloudFormationPlugin.AWS_ACCESS_KEY_ID_KEY;
+import static org.opentosca.toscana.plugins.cloudformation.CloudFormationPlugin.AWS_REGION_DEFAULT;
+import static org.opentosca.toscana.plugins.cloudformation.CloudFormationPlugin.AWS_REGION_KEY;
 import static org.opentosca.toscana.plugins.cloudformation.CloudFormationPlugin.AWS_SECRET_KEY_KEY;
 
 public class CloudFormationLifecycleTest extends BaseUnitTest {
@@ -46,8 +49,13 @@ public class CloudFormationLifecycleTest extends BaseUnitTest {
         when(context.getLogger((Class<?>) any(Class.class))).thenReturn(LoggerFactory.getLogger("Dummy Logger"));
         when(context.getModel()).thenReturn(effectiveModel);
         Map<String, String> properties = new HashMap<>();
-        properties.put(AWS_ACCESS_KEY_ID_KEY, System.getenv("AWS_ACCESS_KEY"));
-        properties.put(AWS_SECRET_KEY_KEY, System.getenv("AWS_SECRET_KEY"));
+        String accessKey = System.getenv("AWS_ACCESS_KEY");
+        String secretKey = System.getenv("AWS_SECRET_KEY");
+        Assume.assumeNotNull(accessKey);
+        Assume.assumeNotNull(secretKey);
+        properties.put(AWS_ACCESS_KEY_ID_KEY, accessKey);
+        properties.put(AWS_SECRET_KEY_KEY, secretKey);
+        properties.put(AWS_REGION_KEY, AWS_REGION_DEFAULT);
         when(context.getProperties()).thenReturn(mock(PropertyInstance.class));
         when(context.getProperties().getPropertyValues()).thenReturn(properties);
         cloudFormationLifecycle = new CloudFormationLifecycle(context);
