07ffa0739a9c590531a4f6051ea436034c92485b
Marvin Munoz Baron <mmunozba@outlook.com>
2018-01-26 18:43:02 +0100

    Use excplicit imports and access modifiers

--- server/src/main/java/org/opentosca/toscana/plugins/cloudformation/CloudFormationModule.java
+++ server/src/main/java/org/opentosca/toscana/plugins/cloudformation/CloudFormationModule.java
@@ -7,10 +7,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import com.scaleset.cfbuilder.core.*;
 import org.opentosca.toscana.core.plugin.PluginFileAccess;
 
 import com.amazonaws.auth.AWSCredentials;
+import com.scaleset.cfbuilder.core.Fn;
+import com.scaleset.cfbuilder.core.Module;
+import com.scaleset.cfbuilder.core.Parameter;
+import com.scaleset.cfbuilder.core.Resource;
+import com.scaleset.cfbuilder.core.Template;
 import com.scaleset.cfbuilder.ec2.Instance;
 import com.scaleset.cfbuilder.ec2.UserData;
 import com.scaleset.cfbuilder.ec2.metadata.CFNInit;
@@ -27,13 +31,13 @@ public class CloudFormationModule extends Module {
     public static final String MODE_500 = "000500";
     public static final String MODE_644 = "000644";
     public static final String OWNER_GROUP_ROOT = "root";
+    public static final String KEYNAME = "KeyName";
     
     // KeyName is a default input value
     private static final String KEYNAME_DESCRIPTION = "Name of an existing EC2 KeyPair to enable SSH access to the " +
         "instances";
     private static final String KEYNAME_TYPE = "AWS::EC2::KeyPair::KeyName";
     private static final String KEYNAME_CONSTRAINT_DESCRIPTION = "must be the name of an existing EC2 KeyPair.";
-    static final String KEYNAME = "KeyName";
     private static final String USERDATA_NAME = "Join";
     private static final String USERDATA_DELIMITER = "";
     private static final String[] USERDATA_CONSTANT_PARAMS = {
