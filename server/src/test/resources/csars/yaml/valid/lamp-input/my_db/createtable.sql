fbe6aa807ac6c778270db498ae5fb6e76bc590fa
Christian Müller <cmueller.dev@gmail.com>
2018-01-31 21:28:53 +0100

    Remove Redundant Models

--- server/src/test/resources/csars/yaml/valid/lamp-input/my_db/createtable.sql
+++
@@ -1,7 +0,0 @@
--- noinspection SqlNoDataSourceInspectionForFile
-
-create table tasks
-   (id int not null unique auto_increment,
-      task varchar(255),
-      primary key (id)
-   );
--- server/src/test/resources/csars/yaml/valid/lamp-noinput/my_app/create_myphpapp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo mv myphpapp.php /var/www/html/
-sudo mv mysql-credentials.php /var/www/html/
-
