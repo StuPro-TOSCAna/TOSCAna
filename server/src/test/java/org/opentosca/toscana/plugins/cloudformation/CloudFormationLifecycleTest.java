c20722e4624dee363f0f2f9030c653cfc4f07cff
Manuel Kloppenburg <mklopp@outlook.de>
2018-02-02 01:20:34 +0100

    Fix test

--- server/src/test/java/org/opentosca/toscana/plugins/cloudformation/CloudFormationLifecycleTest.java
+++ server/src/test/java/org/opentosca/toscana/plugins/cloudformation/CloudFormationLifecycleTest.java
@@ -1,14 +1,11 @@
 package org.opentosca.toscana.plugins.cloudformation;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.opentosca.toscana.core.BaseUnitTest;
 import org.opentosca.toscana.core.plugin.PluginFileAccess;
 import org.opentosca.toscana.core.testdata.TestCsars;
 import org.opentosca.toscana.core.transformation.TransformationContext;
-import org.opentosca.toscana.core.transformation.logging.Log;
 import org.opentosca.toscana.core.transformation.properties.PropertyInstance;
 import org.opentosca.toscana.model.EffectiveModel;
 import org.opentosca.toscana.model.EffectiveModelFactory;
@@ -50,16 +47,14 @@ public class CloudFormationLifecycleTest extends BaseUnitTest {
         when(context.getPluginFileAccess()).thenReturn(access);
         when(context.getLogger((Class<?>) any(Class.class))).thenReturn(LoggerFactory.getLogger("Dummy Logger"));
         when(context.getModel()).thenReturn(effectiveModel);
-        Map<String, String> properties = new HashMap<>();
         String accessKey = System.getenv("AWS_ACCESS_KEY");
         String secretKey = System.getenv("AWS_SECRET_KEY");
         Assume.assumeNotNull(accessKey);
         Assume.assumeNotNull(secretKey);
-        properties.put(AWS_ACCESS_KEY_ID_KEY, accessKey);
-        properties.put(AWS_SECRET_KEY_KEY, secretKey);
-        properties.put(AWS_REGION_KEY, AWS_REGION_DEFAULT);
         when(context.getProperties()).thenReturn(mock(PropertyInstance.class));
-        when(context.getProperties().getPropertyValues()).thenReturn(properties);
+        when(context.getProperties().getOrThrow(AWS_ACCESS_KEY_ID_KEY)).thenReturn(accessKey);
+        when(context.getProperties().getOrThrow(AWS_SECRET_KEY_KEY)).thenReturn(secretKey);
+        when(context.getProperties().getOrThrow(AWS_REGION_KEY)).thenReturn(AWS_REGION_DEFAULT);
         cloudFormationLifecycle = new CloudFormationLifecycle(context);
     }
 
