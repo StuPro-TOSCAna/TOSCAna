88f2c658783576f4849591c22748623e44c0c358
Heiko Nickerl <dev@heiko-nickerl.com>
2018-02-02 19:36:19 +0100

    Reintegrate winery parser for validation

--- server/src/main/java/org/opentosca/toscana/core/csar/CsarServiceImpl.java
+++ server/src/main/java/org/opentosca/toscana/core/csar/CsarServiceImpl.java
@@ -1,13 +1,19 @@
 package org.opentosca.toscana.core.csar;
 
+import java.io.File;
 import java.io.InputStream;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
+import org.opentosca.toscana.core.parse.EntrypointDetector;
 import org.opentosca.toscana.core.parse.InvalidCsarException;
+import org.opentosca.toscana.core.transformation.logging.Log;
 import org.opentosca.toscana.core.util.LifecyclePhase;
 import org.opentosca.toscana.model.EffectiveModelFactory;
 
+import org.eclipse.winery.yaml.common.exception.MultiException;
+import org.eclipse.winery.yaml.common.reader.yaml.Reader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,20 +50,31 @@ public class CsarServiceImpl implements CsarService {
 
     // this call is expensive, use with care
     private void validate(Csar csar) throws InvalidCsarException {
+        Log log = csar.getLog();
+        Logger logger = log.getLogger(getClass());
         LifecyclePhase validatePhase = csar.getLifecyclePhase(Csar.Phase.VALIDATE);
         validatePhase.setState(LifecyclePhase.State.EXECUTING);
         logger.info("Validating csar '{}'", csar.getIdentifier());
-        logger.debug("  > Validating csar with winery parser", csar.getIdentifier());
-        // TODO integrate winery parser as validator
-        // test whether EffectiveModel can get created without throwing an error
+        logger.debug("  > Validating TOSCA template", csar.getIdentifier());
+        File entryPoint = new EntrypointDetector(log).findEntryPoint(csar.getContentDir());
+        try {
+            Reader.getReader().parse(Paths.get(csar.getContentDir().toString()), Paths.get(entryPoint.toString()));
+            logger.info("Template validation successful");
+        } catch (MultiException e) {
+            logger.error("Template validation failed", e);
+            throw new InvalidCsarException(log);
+        }
+
         try {
             // test if conversion does evoke errors
-            logger.info("  > Constructing service template for validation", csar.getIdentifier());
+            logger.info("  > Constructing model from TOSCA template");
             effectiveModelFactory.create(csar);
             validatePhase.setState(LifecyclePhase.State.DONE);
+            logger.info("Model construction successful");
         } catch (Exception e) {
             validatePhase.setState(LifecyclePhase.State.FAILED);
-            throw new InvalidCsarException(csar.getLog());
+            logger.error("Model construction failed", csar.getIdentifier(), e);
+            throw new InvalidCsarException(log);
         }
     }
 
