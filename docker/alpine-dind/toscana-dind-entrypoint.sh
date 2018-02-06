40c23c52645d58f6a49d68b16eff3153afd3f17a
Christian Müller <cmueller.dev@gmail.com>
2018-02-06 20:27:54 +0100

    Add While True loop to automatically restart dockerd if it crashes (#508)

--- docker/alpine-dind/toscana-dind-entrypoint.sh
+++ docker/alpine-dind/toscana-dind-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-dockerd &
+
+# Launch Dockerd
+docker_logfile=/var/log/dockerd.log
+touch $docker_logfile
+while true; do dockerd | tee -a $docker_logfile; done &
+
+# Launch Toscana
 logfile=/var/log/toscana.log
 touch $logfile
 java -jar server.jar | tee -a $logfile
