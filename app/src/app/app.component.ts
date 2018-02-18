030f0f9da5bf4ca53c91edb36715474646d2ed3a
Nick Fode <nickfode@posteo.de>
2018-02-18 19:29:51 +0100

    Add health info to the navbar
    resolves #537

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -3,6 +3,8 @@ import {RouteHandler} from './handler/route/route.service';
 import {Csar} from './model/csar';
 import {PlatformsProvider} from './providers/platforms/platforms.provider';
 import {CsarProvider} from './providers/csar/csar.provider';
+import {HealthService} from './providers/health/health.service';
+import {IntervalObservable} from 'rxjs/observable/IntervalObservable';
 
 @Component({
     selector: 'app-root',
@@ -10,15 +12,35 @@ import {CsarProvider} from './providers/csar/csar.provider';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-
+    status = 'RUNNING';
+    memory = '6GB/10GB';
     csars: Csar[] = [];
     listNotEmpty = false;
 
-    constructor(private platformsProvider: PlatformsProvider, private csarProvider: CsarProvider, private routeHandler: RouteHandler) {
+    constructor(private healthProvider: HealthService, private platformsProvider: PlatformsProvider,
+                private csarProvider: CsarProvider, private routeHandler: RouteHandler) {
+    }
+
+    loadHealthStats() {
+        IntervalObservable.create(2000).subscribe(() => {
+            this.healthProvider.getHealthStatus().subscribe(data => {
+                if (data.transformer.running_transformations.length === 0) {
+                    this.status = 'IDLING';
+                } else {
+                    this.status = 'RUNNING';
+                }
+                this.memory = this.convertToGb(data.diskSpace.total - data.diskSpace.free) + '/' + this.convertToGb(data.diskSpace.total) +
+                    ' GB';
+            });
+        });
+    }
+
+    convertToGb(bytes: number) {
+        const i = Math.floor(Math.log(bytes) / Math.log(1024));
+        return (bytes / Math.pow(1024, Math.floor(i))).toFixed(1);
     }
 
     async ngOnInit() {
-
         await this.platformsProvider.loadPlatforms();
         await this.csarProvider.loadCsars();
         await this.csarProvider.csars.subscribe(async csars => {
@@ -31,6 +53,7 @@ export class AppComponent implements OnInit {
                 this.routeHandler.close();
             }
         });
+        this.loadHealthStats();
     }
 
 }
