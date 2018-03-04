638db4df224979bfeff352dc5ff07affda947141
Christian Müller <cmueller.dev@gmail.com>
2018-03-04 21:01:33 +0100

    Update Travis.yml file to include minikube instructions - Run Minikube directly within travis - Add Wait for travis script

--- docker/k8s-testing/extract-certs.sh
+++ docker/k8s-testing/extract-certs.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 current_directory=$(pwd)
-./kubectl config set-cluster minikube --server=https://localhost:8443 --certificate-authority=${current_directory}/ca.crt 
-./kubectl config set-context minikube --cluster=minikube --user=minikube 
-./kubectl config set-credentials minikube --client-key=${current_directory}/client.key --client-certificate=${current_directory}/client.crt
+kubectl config set-cluster minikube --server=https://localhost:8443 --certificate-authority=${current_directory}/ca.crt 
+kubectl config set-context minikube --cluster=minikube --user=minikube 
+kubectl config set-credentials minikube --client-key=${current_directory}/client.key --client-certificate=${current_directory}/client.crt
 
 
 docker exec -it test cat /root/.minikube/ca.crt > ca.crt
