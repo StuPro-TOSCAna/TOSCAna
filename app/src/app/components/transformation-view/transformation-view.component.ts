ec91f181248ba81ee68276f1e7255cc73ab7abe7
Nick Fode <nick@die-fodes.de>
2018-03-29 00:33:46 +0200

    Add run script download button

--- app/src/app/components/transformation-view/transformation-view.component.ts
+++ app/src/app/components/transformation-view/transformation-view.component.ts
@@ -16,7 +16,7 @@ import {MessageService} from '../../services/message.service';
 import {ClientCsarsService} from '../../services/csar.service';
 import {getColorForLifecyclePhase} from '../../helper/helper';
 import TransformationStateEnum = TransformationResponse.StateEnum;
-import  LifecycleStateEnum = LifecyclePhase.StateEnum;
+import {saveAs} from 'file-saver';
 
 @Component({
     selector: 'app-transformation-view',
@@ -117,6 +117,25 @@ export class TransformationViewComponent implements OnInit, OnDestroy {
         });
     }
 
+    downloadScript() {
+        const folder = this.csarId + '-' + this.platform;
+        let result = '#!/bin/bash\n';
+        result += 'timestamp=$(date +%s)\n';
+        result += 'folder=' + folder + '-$timestamp\n';
+        result += 'mkdir $folder\n';
+        result += 'cd $folder\n';
+        result += 'curl ' + this.url + ' --output $folder.csar\n';
+        result += 'unzip $folder.csar -d unzipped\n';
+        result += 'bash unzipped/output/scripts/deploy.sh\n';
+        result += 'cd ../\n';
+        result += 'rm -rf $folder\n';
+        const file = new File([result], 'run.sh', {type: 'text/plain;charset=ascii'});
+        saveAs(file);
+    }
+
+    checkIfTransformationFailed() {
+        return this.transformation.state === TransformationStateEnum.ERROR;
+    }
     ngOnDestroy() {
         if (!isNullOrUndefined(this.observable)) {
             this.transformationDone = true;
